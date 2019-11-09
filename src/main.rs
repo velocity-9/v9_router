2429d4389f9786824b3ebf8a0d51d44503c39ea4
Pat Cody <patcody028@gmail.com>
2019-11-08 19:23:05 -0500

    Cleaned up main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,36 @@
 //I'd like the most pedantic warning level
-#![warn(clippy::pedantic, clippy::needless_borrow)]
-// But I don't care about these ones for now (most applicable since the code isn't fleshed out)
-#![allow(clippy::module_name_repetitions)]
-#![allow(clippy::needless_pass_by_value)]
+#![warn(
+    clippy::cargo,
+    clippy::needless_borrow,
+    clippy::pedantic,
+    clippy::redundant_clone
+)]
+// But I don't care about these ones
+#![allow(
+    clippy::cast_precision_loss,     // There is no way to avoid this precision loss
+    clippy::module_name_repetitions, // Sometimes clear naming calls for repetition
+    clippy::multiple_crate_versions,  // There is no way to easily fix this without modifying our dependencies
+    clippy::needless_pass_by_value // FIXME: Remove once the code is in a better state 
+)]
 
 #[macro_use]
 extern crate failure;
 #[macro_use]
 extern crate log;
-
 extern crate serde;
-
-use std::env;
-use std::sync::Arc;
-
-use crate::request_handler::HttpRequestHandler;
 
 mod error;
 mod request_handler;
 mod router;
 mod server;
 
+use std::env;
+use std::sync::Arc;
+
+use crate::request_handler::HttpRequestHandler;
+
 fn main() {
-    let is_development_mode = dbg!(env::args()).any(|arg| arg == "--development");
+    let is_development_mode = env::args().any(|arg| arg == "--development");
     if is_development_mode {
         println!("Starting in development mode");
     }
