68bd92f3374368ce3937353c1879085ab1935261
Others <gregorpeach@gmail.com>
2020-01-01 20:38:11 -0800

    Implement component retry properly (#6)
    * Quick fix to always update worker map
    
    * Quick fix for invalid URLs
    
    * Implement component retry properly
    Now we retry if we 404 or if we can't find a worker. Only one
    retry per error condition is allowed.
    
    The alternative here is just to call `update_workers` on a fast
    period. This has the advantage of removing a ton of code. However,
    in case of a read storm, it could cause requests to erroneously
    fail.
    
    I also wonder if there is a locking strategy that would give us
    more faith in the state of the worker map. This code lets it
    change from under us -- which could cause unnessesary calls to
    `update_workers`. However, we probably don't want to hold the lock
    across a call to the worker (which could take an arbitrarly long
    time), so strategies in this direction complicate the code
    somewhat.
    
    * Fix TODO in router.rs
    
    Co-authored-by: Pat Cody <patcody028@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -9,6 +9,7 @@ pub enum RouterError {
     InternalJsonHandling(serde_json::Error),
     InvalidRequest(reqwest::Error),
     InvalidUtf8(Utf8Error),
+    PathNotFound(String),
 }
 
 impl Display for RouterError {
@@ -28,6 +29,10 @@ impl Display for RouterError {
 
             Self::InvalidUtf8(e) => {
                 write!(f, "RouterError, caused by internal utf8 decode error: {}", e)?;
+            }
+
+            Self::PathNotFound(p) => {
+                write!(f, "RouterError, invalid path: {}", p)?;
             }
         }
         Ok(())
@@ -60,9 +65,15 @@ impl From<Utf8Error> for RouterError {
 
 impl Into<Response<Body>> for RouterError {
     fn into(self) -> Response<Body> {
+        let msg = self.to_string();
+        let error_code = match self {
+            Self::PathNotFound(_) => 404,
+            _ => 532,
+        };
+
         Response::builder()
-            .status(532)
-            .body(Body::from(self.to_string()))
+            .status(error_code)
+            .body(Body::from(msg))
             .unwrap()
     }
 }
